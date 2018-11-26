911b4c72fbbb6f2e51f155c458644fbf7a719935
Adrian Bindiu <adrian.bindiu@outlook.com>
2018-11-26 11:20:23 +0000

    formatted + use class

--- extension.js
+++ extension.js
@@ -1,68 +1,79 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 const fs = require('fs');
 const path = require('path');
 
 const TEMPLATE_FOLDER = '.vs-templates';
 const TEMPLATE_NAME_PATTERN = '{{Name}}';
+const PLACEHOLDERS = {
+    SELECT_TEMPLATE: 'Select your template...',
+    TEMPLATE_NAME: 'Enter your template name...',
+    CREATE_FOLDER: 'Create folder for template?'
+};
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-function activate(context) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "vs-generator" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.generateVsTemplate', function (target) {
-		console.log("​activate -> target", target)
+class VSTemplateExtension {
+    onStart(target) {
         const workspacePath = vscode.workspace.workspaceFolders[0].uri.path;
         const templatesFolder = path.join(workspacePath, TEMPLATE_FOLDER);
         const targetPath = target.path;
+        const vswindow = vscode.window;
 
         if (fs.existsSync(templatesFolder)) {
             const templates = fs.readdirSync(templatesFolder);
-            // The code you place here will be executed every time your command is executed
 
-            vscode.window.showQuickPick(templates, { placeHolder : 'Select your template...' }).then(userInput => {
-               vscode.window.showInputBox({ placeHolder: 'Enter your template name...' }).then(templateName => {
-                    vscode.window.showQuickPick(['yes', 'no'], { placeHolder: 'Create folder for template?' }).then(withFolderAnswer => {
-                        const withFolder = withFolderAnswer === 'yes';
+            vswindow
+                .showQuickPick(templates, {placeHolder: PLACEHOLDERS.SELECT_TEMPLATE})
+                .then(userInput => {
+                    vswindow
+                        .showInputBox({placeHolder: PLACEHOLDERS.TEMPLATE_NAME})
+                        .then(templateName => {
+                            vswindow.showQuickPick([
+                                'yes', 'no'
+                            ], {placeHolder: PLACEHOLDERS.CREATE_FOLDER}).then(withFolderAnswer => {
+                                const withFolder = withFolderAnswer === 'yes';
 
-                        const createdTemplateFolder = path.join(targetPath, templateName);
+                                const createdTemplateFolder = path.join(targetPath, templateName);
 
-                        if (withFolder)
-                            fs.mkdirSync(createdTemplateFolder);
+                                if (withFolder)
+                                    fs.mkdirSync(createdTemplateFolder);
 
-                        const selectedTemplateFolder = path.join(templatesFolder, userInput);
-                        const templateFiles = fs.readdirSync(selectedTemplateFolder);
+                                const selectedTemplateFolder = path.join(templatesFolder, userInput);
+                                const templateFiles = fs.readdirSync(selectedTemplateFolder);
 
-                        templateFiles.map(file => {
-                            const fileContent = fs.readFileSync(path.join(selectedTemplateFolder, file));
-                            const regex = new RegExp(TEMPLATE_NAME_PATTERN, "g");
-                            const content = fileContent.toString().replace(regex, templateName);
+                                templateFiles.map(file => {
+                                    const fileContent = fs.readFileSync(path.join(selectedTemplateFolder, file));
+                                    const regex = new RegExp(TEMPLATE_NAME_PATTERN, "g");
+                                    const content = fileContent
+                                        .toString()
+                                        .replace(regex, templateName);
 
-                            fs.writeFileSync(path.join(withFolder ? createdTemplateFolder : targetPath, file.replace(TEMPLATE_NAME_PATTERN, templateName)), content);
-                        });
-                        // Display a message box to the user
-                        vscode.window.showInformationMessage(`Created a new "${userInput}" template`);
-                    });
+                                    fs.writeFileSync(path.join(withFolder
+                                        ? createdTemplateFolder
+                                        : targetPath, file.replace(TEMPLATE_NAME_PATTERN, templateName)), content);
+                                });
 
-               })
-            });
+                                vswindow.showInformationMessage(`Created a new "${userInput}" template`);
+                            });
+
+                        })
+                });
         }
+    }
 
-    });
+    activate(context) {
+        let disposable = vscode
+            .commands
+            .registerCommand('extension.generateVsTemplate', this.onStart);
+        context
+            .subscriptions
+            .push(disposable);
+    }
 
-    context.subscriptions.push(disposable);
+    deactivate() {}
 }
-exports.activate = activate;
 
-// this method is called when your extension is deactivated
-function deactivate() {
-}
-exports.deactivate = deactivate;
+const extension = new VSTemplateExtension();
+
+exports.activate = extension
+    .activate
+    .bind(extension);
+exports.deactivate = extension.deactivate;
